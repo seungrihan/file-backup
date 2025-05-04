e5299b69d3573206323f2eddb77330e1a25de872
seungri han <65847414+seungrihan@users.noreply.github.com>
2025-05-05 03:01:04 +0900

    Update backup.py
    불필요 함수 삭제 및 옵션 추가

--- schedule/backup.py
+++ schedule/backup.py
@@ -4,32 +4,28 @@ import shutil
 import zipfile
 from tqdm import tqdm
 
-# "node_modules" 디렉토리를 제외하기 위한 함수
-def exclude_directory(dir, contents):
-    # "node_modules" 디렉토리를 제외한다.
-    return ["node_modules"]
+def backup_directory(source_dir, current_directory, exclude_dirs=None):
+    if exclude_dirs is None:
+        exclude_dirs = []
 
-
-# 디렉토리를 백업하는 함수
-def backup_directory(source_dir, current_directory):
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     dest_dir = os.path.join(current_directory, f"backup_{timestamp}")
     backup_file = os.path.join(current_directory, f"backup_{timestamp}.zip")
 
     try:
-        # 복사 대상 파일 목록 수집 및 총 크기 계산
+        # 복사 대상 파일 목록 수집 및 크기 계산
         file_list = []
         total_size = 0
         for root, dirs, files in os.walk(source_dir):
-            if "node_modules" in dirs:
-                dirs.remove("node_modules")
+            # 제외할 디렉토리를 현재 디렉토리에서 제거
+            dirs[:] = [d for d in dirs if d not in exclude_dirs]
             for file in files:
                 file_path = os.path.join(root, file)
                 file_size = os.path.getsize(file_path)
                 file_list.append((file_path, file_size))
                 total_size += file_size
 
-        # 복사 진행 tqdm
+        # 복사
         with tqdm(total=total_size, unit="B", unit_scale=True, desc="Copying files") as copy_progress:
             for file_path, file_size in file_list:
                 rel_path = os.path.relpath(file_path, source_dir)
@@ -38,7 +34,7 @@ def backup_directory(source_dir, current_directory):
                 shutil.copy2(file_path, dest_path)
                 copy_progress.update(file_size)
 
-        # 압축 대상 수집 및 크기 계산
+        # 압축 대상 목록
         zip_file_list = []
         zip_total_size = 0
         for root, _, files in os.walk(dest_dir):
@@ -48,7 +44,7 @@ def backup_directory(source_dir, current_directory):
                 zip_file_list.append((file_path, file_size))
                 zip_total_size += file_size
 
-        # 압축 진행 tqdm
+        # 압축
         with zipfile.ZipFile(backup_file, "w", zipfile.ZIP_DEFLATED) as zipf, \
              tqdm(total=zip_total_size, unit="B", unit_scale=True, desc="Compressing files") as zip_progress:
             for file_path, file_size in zip_file_list:
