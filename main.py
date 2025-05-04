4c6b02f1b31a4aa5863ce248f2422c8960b663ba
seungri han <65847414+seungrihan@users.noreply.github.com>
2025-05-05 03:02:10 +0900

    Update main.py
    backup_directory 옵션 추가

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     source_directory = "../study"
     destination_directory = "/Users/seungrihan/Google Drive/내 드라이브/Backup"
     current_directory = os.getcwd()
-    backup_directory(source_directory, current_directory)
+    backup_directory(source_directory, current_directory, exclude_dirs=["node_modules"])
     move_zip_files(current_directory, destination_directory)
     delete_oldest_backup_file(destination_directory)
     
